3bcb02f291f7e2878daec4e13571a5e4ea1883a1
Ludwring Liccien <ludwring.liccien@gmail.com>
2018-05-15 18:24:00 -0500

    feat(global) SW auto refresh and Caching Policy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Service Worker
+import { SwUpdate } from '@angular/service-worker';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  constructor(
+    private swUpdate: SwUpdate,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    // Validate browser SW support and auto refresh of the app
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        window.location.reload();
+      });
+    }
+  }
+
+
 }
